6c0d328c00680455bcae3552ce70bcfa40b03063
wiqqicode <wiqx.prof.se@gmail.com>
2024-11-04 06:30:01 +0500

    1st commit

---
+++ data/eyoom.seocfg.php
@@ -0,0 +1,12 @@
+<?php
+if (!defined('_EYOOM_')) exit;
+$seocfg = array(
+	"mt_verification" => "",
+	"mt_title" => "eyoom",
+	"mt_keywords" => "",
+	"mt_description" => "",
+	"mt_robots" => "",
+	"mt_author" => "",
+	"mt_publisher" => "",
+	"mt_img_url" => "",
+);
